f3dabbac6df39d06589e0d145c20bf5676293b50
victorbaptistado <victorbaptistado@gmail.com>
2021-12-13 12:59:46 +0000

    try on frame animation CharacterImage

--- src/Components/Character.js
+++ src/Components/Character.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Spring from "react-spring";
+import { useSpring, animated, useSprings } from "react-spring";
 import { useState, useRef, useEffect } from 'react';
 import Idle00 from "../images/Characters/Idle__000.png";
 import Idle01 from "../images/Characters/Idle__001.png";
@@ -11,67 +11,76 @@ import Idle06 from "../images/Characters/Idle__006.png";
 import Idle07 from "../images/Characters/Idle__007.png";
 import Idle08 from "../images/Characters/Idle__008.png";
 import Idle09 from "../images/Characters/Idle__009.png";
+import Dead00 from "../images/Characters/Dead__000.png";
 
 
-
-const Character = ({}) => {
-
-
+const Character = ({ }) => {
 
     const [horizontal, setHorizontal] = useState(0);
+ 
 
-   
-    const prevCount = useRef()
-   
-    function go(event){
-      
-       
-        setHorizontal(
-        
-        function handle(){
-        prevCount.current = horizontal;
-        if(event.key == "d")
-        return horizontal + 0.3;
-
-        else if(event.key == "a")
-        return horizontal - 0.3;
-        else return horizontal;
-        }
-        )  }
-
-    const [idle, setIdle] = useState("");
+    const prevCount = useRef();
     
+    function walkHorizontal(event) {
 
-        useEffect(() => {
-        
-        setIdle(
-        <Spring 
-        from={{Idle00}}
-        to={{ Idle01}}
-        to={{ Idle02}}
-        to={{ Idle03}}
-        to={{ Idle04}}
-        to={{ Idle05}}
-        to={{ Idle06}}
-        to={{ Idle07}}
-        to={{ Idle08}}
-        to={{ Idle09}}
-        >  
-        </Spring>
-        )}
-        , [])
+        setHorizontal(
 
-  
-    return ( 
-        <div tabindex="0" onKeyPress={go} >
-      
-      
-             <img className="character" style={{marginLeft:`${horizontal}%`}} src={idle} />
-        
-        
-  
-        
+            function handle() {
+                prevCount.current = horizontal;
+                if (event.key == "d")
+                    return horizontal + 0.3;
+
+                else if (event.key == "a")
+                    return horizontal - 0.3;
+                else return horizontal;
+            }
+        )
+    }
+
+    const [idles, setIdle] = useState([
+    
+        {
+        id: 0,
+        img:  <img className="character" src={Idle00} />,
+        },
+        {
+        id: 1,
+        img: <img className="character" src={Dead00}/>,
+        },
+        {
+        id: 2,
+        img: <img className="character" src={Idle02}/>,
+        },
+      ]
+
+    )
+
+/*    const idlesFilt = idles.filter(idle => idle.id === count)*/
+
+/*
+    const lets = useSpring({
+        loop: true,
+        from: idles.img,
+        to: idles.img,    
+    })
+  */  
+    return (
+     
+       
+        <div className='characterImage' tabindex="0" onKeyPress={walkHorizontal} >
+       
+
+        <animated.img className="character" style={{marginLeft: `${horizontal}%`}} src={Idle00}  />  
+    {/*    {idlesFilt.map((idle) =>
+      <>
+        {idle.img}
+        </>
+    )}*/}
         </div>
+        
+
+
+    
     )
 }
 
